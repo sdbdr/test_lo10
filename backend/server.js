de914d03c92f5d4b8a76614a3625db7e4a68ea17
Huu Khai NGUYEN (luciffer1904) <khainguyenhuu121@gmail.com>
2023-05-30 10:29:08 +0200

    Simple authentication with success

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
-
 const express = require("express");
 const session = require("express-session");
 
 const app = express();
-const port = 5000;
+const port = 8080;
 
 const generateSessionSecret = () => {
   return crypto.randomBytes(32).toString("hex");
@@ -27,27 +26,37 @@ const cors = require("cors");
 app.use(cors());
 
 // Define a route to handle the incoming data
+// testuser : password123
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
+
+const mockUser = {
+  id: 1,
+  username: "testuser",
+  password: "password123", // -> need to hash password: 'password123', but to simplify the things, just let it like this
+};
 app.post("/api/login", async (req, res) => {
   const { username, password } = req.body;
-
-  // Authenticate user here by checking their credentials against your user database
-  const user = await User.findOne({ username });
-  if (!user) {
+  if (username !== mockUser.username) {
+    console.log("Username failed")
     return res
       .status(401)
       .json({ success: false, message: "Invalid credentials" });
   }
-
-  const passwordMatch = await bcrypt.compare(password, user.password);
+  
+  // const passwordMatch = await bcrypt.compare(password, mockUser.password);
+  const passwordMatch = password === mockUser.password;
   if (!passwordMatch) {
+    console.log("pass failed")
     return res
       .status(401)
       .json({ success: false, message: "Invalid credentials" });
   }
-
-  // Set a session cookie with the authenticated user's ID
-  req.session.userId = user.id;
-  res.json({ success: true, user });
+  req.session.userId = mockUser.id;
+  res.json({ success: true, user: mockUser });
 });
 
 app.post("/api/trips", (req, res) => {
--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -22,7 +22,7 @@ function LoginForm({ onLogin }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/login", {
+    const response = await fetch("http://localhost:8080/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
